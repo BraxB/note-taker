67d8c7c6e0d43d444d1decc712d9a657f34112db
BraxB <braxb92@gmail.com>
2021-05-01 16:29:10 -0400

    added heroku and changed port to dynamic

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = 8080;
+var PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
